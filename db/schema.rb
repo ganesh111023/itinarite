298807353b75b1a554a6ea2f0eb0fc14504f8ed3
Pawan shrivas <pkshrivas88@gmail.com>
2016-05-13 23:38:30 +0530

    misc changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413195536) do
+ActiveRecord::Schema.define(version: 20160507175345) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id", limit: 4
+    t.integer  "followed_id", limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
