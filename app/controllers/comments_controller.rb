44b45cb1c3b6129ca2f2e132c87ebd0434e2cd70
Pawan shrivas <pkshrivas88@gmail.com>
2016-10-04 16:56:20 +0530

    optimize code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
+  before_action :find_trip
+  before_action :find_comment, only: [:destroy, :update]
 
   #/trips/:trip_id/comments
   def create
-    @trip = current_user.trips.find params[:trip_id]
     comment = @trip.comments.new({description: params[:description] })
     comment.user_id = current_user.id
     respond_to do |format|
@@ -13,22 +14,27 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @trip = current_user.trips.find params[:trip_id]
-    comment = @trip.comments.find(params[:id])
     respond_to do |format|
-      if comment.destroy
+      if @comment.destroy
          format.js   { }
       end
     end
   end
 
   def update
-    @trip = current_user.trips.find params[:trip_id]
-    comment = @trip.comments.find(params[:id])
     respond_to do |format|
-      if comment.update_column(:description, params[:description])
+      if @comment.update_column(:description, params[:description])
          format.js   { }
       end
     end
   end
+
+  private
+    def find_trip
+      @trip = current_user.trips.find params[:trip_id]
+    end
+    def find_comment
+      find_trip
+      @comment = @trip.comments.find(params[:id])
+    end
 end
