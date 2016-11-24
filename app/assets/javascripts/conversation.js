db37e7909d6f1a2675e8877e671a9ca2ccdf52b7
Pawan shrivas <pkshrivas88@gmail.com>
2016-11-24 10:49:56 +0530

    Add channels for live chat

--- app/assets/javascripts/conversation.js
+++ app/assets/javascripts/conversation.js
@@ -1,11 +1,5 @@
 var ready = function () {
 
-    /**
-     * When the send message link on our home page is clicked
-     * send an ajax request to our rails app with the sender_id and
-     * recipient_id
-     */
-
     $('.start-conversation').click(function (e) {
         e.preventDefault();
 
@@ -17,34 +11,6 @@ var ready = function () {
         });
 
     });
-
-    /**
-     * Used to minimize the chatbox
-     */
-
-    $(document).on('click', '.toggleChatBox', function (e) {
-        e.preventDefault();
-
-        var id = $(this).data('cid');
-        chatBox.toggleChatBoxGrowth(id);
-    });
-
-    /**
-     * Used to close the chatbox
-     */
-
-    $(document).on('click', '.closeChat', function (e) {
-        e.preventDefault();
-
-        var id = $(this).data('cid');
-        chatBox.close(id);
-    });
-
-
-    /**
-     * Listen on keypress' in our chat textarea and call the
-     * chatInputKey in chat.js for inspection
-     */
 
     $(document).on('keydown', '.chatboxtextarea', function (event) {
         var id = $(this).data('cid');
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,8 +6,8 @@ class MessagesController < ApplicationController
     @message = @conversation.messages.build(message_params)
     @message.user_id = current_user.id
     @message.save!
-
-    @path = conversation_path(@conversation)
+    @channel = "/messages/private/#{@conversation.recipient_id}/#{@conversation.sender_id}"
+    @channel1 = "/messages/private/#{@conversation.sender_id}/#{@conversation.recipient_id}"
   end
 
   private
