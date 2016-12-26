28307c8310dc5d5cb0a2d98a8fde43fd681c3f45
Pawan shrivas <pkshrivas88@gmail.com>
2016-12-27 00:26:16 +0530

    destroy conversation message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
 	before_filter :authenticate_user!
+  before_filter :find_conversation
 
   def create
-    @conversation = Conversation.find(params[:conversation_id])
     @message = @conversation.messages.build(message_params)
     @message.user_id = current_user.id
     @message.save!
@@ -10,7 +10,17 @@ class MessagesController < ApplicationController
     @channel1 = "/messages/private/#{@conversation.sender_id}/#{@conversation.recipient_id}"
   end
 
+  def destroy
+    message = @conversation.messages.find_by_id params[:id]
+    message.destroy
+    @channel = "/messages/private/#{@conversation.recipient_id}/#{@conversation.sender_id}"
+  end
+
   private
+
+  def find_conversation
+    @conversation = Conversation.find(params[:conversation_id])
+  end
 
   def message_params
     params.require(:message).permit(:body)
